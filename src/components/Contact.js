80491d922b991475f67b27ff31e419bb81c4b37f
MattMcPhee <matt.imcphee@gmail.com>
2021-10-11 17:24:39 +1000

    Imported my existing site

---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Contact = () => {
+  return <div className='contact'>This is where you can send me an email.</div>
+}
+
+export default Contact
---
+++ src/components/Projects.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Projects = () => {
+  return <div>This is where you can find all my previous projects.</div>
+}
+
+export default Projects
