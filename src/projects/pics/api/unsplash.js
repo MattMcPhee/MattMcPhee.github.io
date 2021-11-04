8932deb599aa1fba366dda31b426e360ddf42819
MattMcPhee <matt.imcphee@gmail.com>
2021-11-04 19:17:51 +1000

    Updates

--- src/projects/pics/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID 3zEkpfaFBz_8LIWPikYdr6iLUIF74pOUgM1t9dw2AVQ'
-  }
-});
